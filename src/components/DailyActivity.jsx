69d14537173ac541f5babf88a3b4b941cf8b3a23
DylanLaptop <dylannicolet@hotmail.com>
2022-06-22 17:39:57 +0400

    Worked on: DailyActivity style

--- src/components/DailyActivity.jsx
+++ src/components/DailyActivity.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis, ResponsiveContainer} from "recharts";
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Tooltip, Legend} from "recharts";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -26,15 +26,18 @@ export default function DailyActivity(){
     return(
         <section className="daily-activity">
             {answer && <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={100} height={100} data={answer.sessions}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="day" />
-                    <YAxis dataKey="kilogram" />
-                    <Bar dataKey="kilogram" />
-                    <Bar dataKey="calories" fill="#FF0101" width="2px" />
+                <BarChart width={100} height={100} data={answer.sessions} barGap={8}>
+                    <CartesianGrid strokeDasharray="3 3" vertical={false}/>
+                    <XAxis dataKey="day" tickMargin={10} />
+                    <YAxis yAxisId="weight" dataKey="kilogram"  orientation="right"  domain={["dataMin - 10","dataMax + 10"]} tickCount={4} axisLine={false} tickMargin={30}/>
+                    <YAxis yAxisId="calories" dataKey="calories"  orientation="left"  domain={["dataMin - 20","dataMax + 20"]} tick={false} axisLine={false}/>
+                    <Tooltip />
+                    <Bar yAxisId="weight" dataKey="kilogram" barSize={10} radius={[10,10,0,0]}/>
+                    <Bar yAxisId="calories" dataKey="calories" fill="#FF0101" width="2px" barSize={10} radius={[10,10,0,0]} name={"calories"}/>
+                    <Tooltip />
+                    <Legend verticalAlign="top" align="right" iconType={"circle"} height={40} payload={[{ value: 'weight (kg)', type: 'circle', id: 'weight' },{ value: 'Burned calories (kCal)', type: 'circle', id: 'ID02' }]} />
                 </BarChart>
             </ResponsiveContainer>}
         </section>
-        
     )
 }
